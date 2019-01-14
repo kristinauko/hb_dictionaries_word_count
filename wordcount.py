d0133b8420d80d2c602dc3bb856d9be3c5cf2fee
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-14 12:24:25 -0800

    Final version

--- wordcount.py
+++ wordcount.py
@@ -3,3 +3,21 @@ def count_words(textfile):
     
     textfile = open(textfile)
 
+    word_count_dict = {}
+
+    for line in textfile:
+        line = line.rstrip()
+        word_list = line.split(" ")
+
+        for word in word_list:
+            word_count_dict[word] = word_count_dict.get(word, 0) + 1
+
+    formatted_count = ""
+
+    for word, count in word_count_dict.items():
+        formatted_count = formatted_count + (f'{word} {count}') + "\n"
+
+    return formatted_count
+
+#print(count_words("test.txt"))
+print(count_words("twain.txt"))
